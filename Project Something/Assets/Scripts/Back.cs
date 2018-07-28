145aa375870466d8e405e149ad6f80132f31de58
Pizzadrengen <39215307+Pizzadrengen@users.noreply.github.com>
2018-07-28 17:41:48 +0200

    xx
    xx

---
+++ Project Something/Assets/Scripts/Back.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Back : MonoBehaviour {
+
+    public GameObject BackUI;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void back()
+    {
+        BackUI.SetActive(true);
+        SceneManager.LoadScene("Arena");
+        Time.timeScale = 1f;
+
+    }
+}
+
