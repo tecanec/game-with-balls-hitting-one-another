8d8d53584c2a7c7fc7194cf8fa5ffef5d89cca9c
Pizzadrengen <39215307+Pizzadrengen@users.noreply.github.com>
2018-08-02 16:05:21 +0200

    d
    d

--- Project Something/Assets/Scripts/Selectlevel.cs
+++ Project Something/Assets/Scripts/Selectlevel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SelectLevel : MonoBehaviour
+public class Selectlevel : MonoBehaviour
 {
 
     public GameObject SelectArenaUI;
