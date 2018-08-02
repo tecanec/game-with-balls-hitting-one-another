b29d3776b817d2f34d0e33259e61c2344a9c89f7
Asger Stig Abildgaard <asgera02@gmail.com>
2018-08-02 15:28:37 +0200

    Audio Slider script

---
+++ Project Something/Assets/Scripts/AudioSlider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class AudioSlider : MonoBehaviour {
+    public Slider slider;
+    public AudioMixer target;
+    public string paramName;
+
+	// Use this for initialization
+	void Start () {
+        float value;
+        target.GetFloat(paramName, out value);
+        slider.value = value;
+
+        slider.onValueChanged.AddListener(delegate { ChangeValue(slider.value); });
+	}
+	
+	public void ChangeValue (float value)
+    {
+        target.SetFloat(paramName, value);
+    }
+}
