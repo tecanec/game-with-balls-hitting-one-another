6b7ce694edec9203643be18d7ab4640f35eedff4
Asger Stig Abildgaard <asgera02@gmail.com>
2018-08-11 20:37:07 +0200

    ny baggrund til The Mixer
    Gratis bugs inkluderet!

--- Project Something/Assets/Scripts/SpinnyThingy.cs
+++ Project Something/Assets/Scripts/SpinnyThingy.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class SpinnyThingy : MonoBehaviour {
     public HingeJoint2D Motor;
     public AnimationCurve SpinSpeedOverTime;
-    public SpriteRenderer Light;
+    public MeshRenderer Light;
+    public int LightMaterialId;
+    public Material MaterialPrototype;
     public Color Blank;
     public Color Accelerating;
 
@@ -13,17 +15,24 @@ public class SpinnyThingy : MonoBehaviour {
 
     float startTime;
 
+    Material material;
+
 	// Use this for initialization
 	void Start () {
         masta = GameMasta.TheMasta;
         startTime = masta.CountDown;
+
+        material = new Material(MaterialPrototype);
+        Light.materials[LightMaterialId] = material;
     }
 	
 	// Update is called once per frame
 	void Update () {
         JointMotor2D motorToChange = Motor.motor;
         motorToChange.motorSpeed = SpinSpeedOverTime.Evaluate((startTime - masta.CountDown) / startTime) * 200;
-        Light.color = Color.Lerp(Blank, Accelerating, (motorToChange.motorSpeed - Motor.motor.motorSpeed) / Time.deltaTime);
+        material.color = Color.Lerp(Blank, Accelerating, (motorToChange.motorSpeed - Motor.motor.motorSpeed) / Time.deltaTime);
+        Debug.Log(motorToChange.motorSpeed);
         Motor.motor = motorToChange;
-	}
+        Debug.Log(Motor.motor.motorSpeed);
+    }
 }
